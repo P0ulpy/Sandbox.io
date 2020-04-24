7a6091e53bc1d0380f37141fe425d0c000a477f7
Diplo <t.hesters@protonmail.com>
2020-04-24 13:41:41 +0200

    Repare MAster

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+
+const path = require('path');
+const RoomsManager = require('./RoomsManager')
+
+app.use(express.static(path.join(__dirname + '/client')));
+
+server.listen(25565);
+
+const roomsManager = new RoomsManager({httpServer: server});
