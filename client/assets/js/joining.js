9f024e7fba2c522ff18c8606e85d8db9b793cab8
Diplo <t.hesters@protonmail.com>
2020-04-26 18:26:13 +0200

    Maj CSS et ejs pour click and join

--- client/assets/js/joining.js
+++ client/assets/js/joining.js
@@ -1,29 +1,23 @@
-class Joining 
-{
-    constructor(config = {}) 
-    {
+class Joining {
+    constructor(config = {}) {
         this.server = config.server || 'http://localhost:80';
 
         this.rooms = {};
-        
+
         this.initJoin();
         this.initCreate();
     }
 
     // Partie Create Room
 
-    initCreate() 
-    {
+    initCreate() {
         this.create_info = document.getElementById('create-info');
-
+        //this.elements = document.getElementsByClassName("joinRoom-room");
         this.create_inputs = document.getElementsByClassName('create-input');
 
-        for(const input of this.create_inputs)
-        {
-            input.addEventListener("keyup", (event) => 
-            {
-                if (event.keyCode === 13) 
-                {
+        for (const input of this.create_inputs) {
+            input.addEventListener("keyup", (event) => {
+                if (event.keyCode === 13) {
                     event.preventDefault();
                     document.getElementById('create-submit').click();
                 }
@@ -33,26 +27,42 @@ class Joining
 
     // Patie Join Room
 
-    initJoin() 
-    {
+    initJoin() {
         this.joinRoom_refreshList = document.getElementById('join-refresh');
+        this.joinRoom_refreshList.addEventListener('click', () => {
 
-        this.joinRoom_refreshList.addEventListener('click', () => 
-        {
-            
         });
 
         this.rooms = document.getElementsByClassName("joinRoom-room");
+        this.joinRoom_Input = document.getElementById("UID");
+        this.joinRoom_Button = document.getElementById("join-submit");
+        for (let i = 0; i < this.rooms.length; i++) {
+            this.rooms[i].addEventListener('click', () => {
 
-        for(const room of this.rooms)
-        {
-            room.addEventListener('click', () => 
-            {
-                console.log('ça clique bien la');
+                let attribute = this.rooms[i].getAttribute("id");
+                Swal.fire({
+                    title: 'Connection à : ' +  attribute,
+                    text: "Vous voulez vous connecter à  " + attribute,
+                    icon: 'question',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: "Rejoindre"
+                  }).then((result) => {
+                    if (result.value) {
+                        this.joinRoom_Input.value = attribute;
+                        this.joinRoom_Button.click();
+                    }
+                  })
+                /*let choice = confirm("Vous voulez vous connecter à  " + attribute);
+                if (choice === true) {
+                    this.joinRoom_Input.value = attribute;
+                    this.joinRoom_Button.click();
+                };*/
 
-                // TODO : code sweet alert
-            });
+            }, false);
         }
+
     }
 }
 
