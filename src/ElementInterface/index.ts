caadf7d781ac597ecf1701bfbbbb4bbbdfda831f
Schrubitteflau <45637360+Schrubitteflau@users.noreply.github.com>
2020-06-19 15:48:19 +0200

    Une bonne fois pour toute

---
+++ src/ElementInterface/index.ts
@@ -0,0 +1,7 @@
+import ElementInterface from "./ElementInterface";
+import ModInterface from "./ModInterface";
+import ModInterfaceDependencies from "./ModInterfaceDependencies";
+import SandboxInterface from "./SandboxInterface";
+
+export default ElementInterface;
+export { ModInterface, ModInterfaceDependencies, SandboxInterface };
