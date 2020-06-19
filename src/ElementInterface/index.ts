f2df80c6f5309b1ea57441296af206b319e759b8
Schrubitteflau <45637360+Schrubitteflau@users.noreply.github.com>
2020-06-19 15:45:43 +0200

    Cleaning via mr propre

--- src/ElementInterface/index.ts
+++
@@ -1,7 +0,0 @@
-import ElementInterface from "./ElementInterface";
-import ModInterface from "./ModInterface";
-import ModInterfaceDependencies from "./ModInterfaceDependencies";
-import SandboxInterface from "./SandboxInterface";
-
-export default ElementInterface;
-export { ModInterface, ModInterfaceDependencies, SandboxInterface };
